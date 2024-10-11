f02d4d61a4d5027fcd62a631b98f18fac0aaa680
tsotne <tsotnemeskhuri@gmail.com>
2024-10-11 21:59:19 +0400

    Add CountryList component

---
+++ src/components/CountryList.jsx
@@ -0,0 +1,32 @@
+import styles from "./CountryList.module.css";
+import Spinner from "./Spinner";
+import Message from "./Message";
+import CountryItem from "./CountryItem";
+
+export default function CountryList({ cities, isLoading }) {
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  if (!cities.length) {
+    return (
+      <Message message="Add your first city by clicking on a city on the map" />
+    );
+  }
+
+  const countries = cities.reduce((acc, city) => {
+    if (!acc.find((country) => country.id === city.country.id)) {
+      acc.push({ id: city.id, country: city.country, emoji: city.emoji });
+    }
+
+    return acc;
+  }, []);
+
+  return (
+    <ul className={styles.countryList}>
+      {countries.map((country) => (
+        <CountryItem key={country.id} country={country} />
+      ))}
+    </ul>
+  );
+}
