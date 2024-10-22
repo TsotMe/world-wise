f43a01aff91349f4f1ef8c064e41d1ed2853eeb6
tsotne <tsotnemeskhuri@gmail.com>
2024-10-22 20:56:41 +0400

    Display separate markers for each city

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,10 +2,12 @@ import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 
 import { useState } from "react";
+import { useCities } from "../contexts/CitiesContext";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 export default function Map() {
   const navigate = useNavigate();
+  const { cities } = useCities();
   const [mapPosition, setMapPosition] = useState([40, 0]);
 
   const [searchParams, setSearchParams] = useSearchParams();
@@ -24,11 +26,24 @@ export default function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
-        <Marker position={mapPosition}>
+        {cities.map((city) => (
+          <Marker
+            key={city.id}
+            position={[city.position.lat, city.position.lng]}
+          >
+            <Popup>
+              <span>
+                {city.emoji}
+                <span>{city.cityName}</span>
+              </span>
+            </Popup>
+          </Marker>
+        ))}
+        {/* <Marker position={mapPosition}>
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
-        </Marker>
+        </Marker> */}
       </MapContainer>
     </div>
   );
